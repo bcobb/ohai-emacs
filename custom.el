d6df375fea2dd5defc0cbf7aed842ceaa853827a
Brian Cobb <bcobb@uwalumni.com>
2015-07-08 13:44:06 -0500

    Include custom.el in my fork

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ohai-personal-taste/paredit t)
+ '(ohai-personal-taste/run-wizard nil)
+ '(ohai-personal-taste/splash (quote daily-otter))
+ '(ohai-personal-taste/style (quote dark))
+ '(ohai-personal-taste/training-wheels t)
+ '(ohai-personal-taste/window-state (quote normal))
+ '(ohai/modules
+   (quote
+    (ohai-appearance ohai-fonts ohai-general ohai-splash ohai-ido ohai-editing ohai-complete ohai-codestyle ohai-dired ohai-project ohai-flycheck ohai-git ohai-orgmode ohai-swiper ohai-help ohai-elisp ohai-html ohai-markdown ohai-javascript))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
