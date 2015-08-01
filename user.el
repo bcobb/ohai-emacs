c8cacfd94adc9c10559852c7ea8c9a34d7e1c067
Brian Cobb <bcobb@uwalumni.com>
2015-08-01 09:14:49 -0500

    Map C-x f to open recent files
    Thanks to devn for this one

--- user.el
+++ user.el
@@ -15,3 +15,20 @@
 (add-hook
  'cider-mode-hook
  #'eldoc-mode)
+
+;; Open recent files in style. (C-x f)
+
+(require 'recentf)
+(setq recentf-max-saved-items 200
+      recentf-max-menu-items 25)
+(recentf-mode +1)
+(setq ido-save-directory-list-file (expand-file-name "~/.emacs.d/.ido.last"))
+
+(defun recentf-ido-find-file ()
+  "Find a recent file using ido."
+  (interactive)
+  (let ((file (ido-completing-read "Choose recent file: " recentf-list nil t)))
+    (when file
+      (find-file file))))
+
+(global-set-key (kbd "C-x f") 'recentf-ido-find-file)
