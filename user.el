67fd610fbf9535d525d83437bd54bf6de10b9ff5
Brian Cobb <bcobb@uwalumni.com>
2015-08-01 09:16:47 -0500

    Fix copy/paste

--- user.el
+++ user.el
@@ -43,3 +43,17 @@
       (find-file file))))
 
 (global-set-key (kbd "C-x f") 'recentf-ido-find-file)
+
+;; Unify copy/paste
+
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
