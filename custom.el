ff164a6467bca0c0752a7a3c7eeba4f0e9a4c9f2
Brian Cobb <bcobb@uwalumni.com>
2017-01-22 10:21:08 -0500

    Update custom.el
    Mainly changes to Ruby, RSpec, and Clojure settings

--- custom.el
+++ custom.el
@@ -3,13 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-revert-verbose nil)
+ '(clojure-defun-indents (quote (fact)))
+ '(clojure-indent-style :always-align)
  '(comment-multi-line t)
  '(css-indent-offset 2)
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
  '(eldoc-print-after-edit nil)
+ '(flycheck-ruby-rubocop-executable nil)
+ '(ido-max-file-prompt-width 0.67)
  '(ido-vertical-define-keys (quote C-n-and-C-p-only))
+ '(imenu-max-item-length 120)
  '(markdown-gfm-downcase-languages t)
  '(markdown-gfm-use-electric-backquote nil)
  '(ohai-personal-taste/paredit t)
@@ -21,7 +27,13 @@
  '(ohai/modules
    (quote
     (ohai-appearance ohai-fonts ohai-general ohai-splash ohai-ido ohai-editing ohai-complete ohai-codestyle ohai-dired ohai-project ohai-flycheck ohai-git ohai-orgmode ohai-swiper ohai-help ohai-elisp ohai-smart-mode-line ohai-html ohai-markdown ohai-javascript ohai-elixir)))
- '(ruby-deep-indent-paren nil))
+ '(rng-nxml-auto-validate-flag nil)
+ '(rspec-use-rake-when-possible nil)
+ '(rspec-use-spring-when-possible nil)
+ '(rspec-use-zeus-when-possible nil)
+ '(ruby-deep-arglist nil)
+ '(ruby-deep-indent-paren nil)
+ '(safe-local-variable-values (quote ((encoding . binary)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
