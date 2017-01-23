17111225d24aa9c17c8dfd651d33e345fb3d1c32
Brian Cobb <bcobb@uwalumni.com>
2017-01-23 16:20:18 -0500

    Only use bundle exec rubocop if rubocop's in the Gemfile
    I'm a little worried about reading Gemfile.lock to run rubocop but we'll
    see how it goes.

--- user.el
+++ user.el
@@ -91,8 +91,14 @@
     (let ((file-path (concat (projectile-project-root) "Gemfile.lock")))
       (file-exists-p file-path))))
 
+(defun bcobb-projectile-project-uses-rubocop-p ()
+  (when (bcobb-projectile-bundler-p)
+    (let ((contents (with-temp-buffer (insert-file-contents (concat (projectile-project-root) "Gemfile.lock"))
+                                      (buffer-string))))
+      (string-match-p "rubocop" contents))))
+
 (defun bcobb-flycheck-rubocop-wrapper (command)
-  (if (bcobb-projectile-bundler-p)
+  (if (bcobb-projectile-project-uses-rubocop-p)
       (append '("bundle" "exec") command)
     command))
 
