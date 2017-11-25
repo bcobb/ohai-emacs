43eec7546807ada92611dd9cf1c1a513c20b8142
Brian Cobb <bcobb@uwalumni.com>
2017-11-25 17:10:12 -0500

    Blindly update custom.el
    - shrug

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-revert-verbose nil)
- '(clojure-defun-indents (quote (fact)))
+ '(clojure-defun-indents (quote (context fact GET POST)))
  '(clojure-indent-style :always-align)
  '(comment-multi-line t)
  '(css-indent-offset 2)
@@ -16,6 +16,7 @@
  '(ido-max-file-prompt-width 0.67)
  '(ido-vertical-define-keys (quote C-n-and-C-p-only))
  '(imenu-max-item-length 120)
+ '(logbook-directory "~/Library/Mobile Documents/com~apple~CloudDocs/logbook")
  '(markdown-gfm-downcase-languages t)
  '(markdown-gfm-use-electric-backquote nil)
  '(ohai-personal-taste/paredit t)
@@ -26,8 +27,13 @@
  '(ohai-personal-taste/window-state (quote normal))
  '(ohai/modules
    (quote
-    (ohai-appearance ohai-fonts ohai-general ohai-splash ohai-ido ohai-editing ohai-complete ohai-codestyle ohai-dired ohai-project ohai-flycheck ohai-git ohai-orgmode ohai-swiper ohai-help ohai-elisp ohai-smart-mode-line ohai-html ohai-markdown ohai-javascript ohai-elixir)))
+    (ohai-appearance ohai-fonts ohai-general ohai-splash ohai-ido ohai-editing ohai-complete ohai-codestyle ohai-dired ohai-project ohai-flycheck ohai-git ohai-orgmode ohai-swiper ohai-help ohai-elisp ohai-smart-mode-line ohai-html ohai-markdown ohai-javascript ohai-purescript ohai-elixir ohai-haskell)))
+ '(package-selected-packages
+   (quote
+    (psc-ide psci flycheck-purescript purescript-mode sql-indent editorconfig rspec-mode elm-mode yard-mode yaml-mode which-key web-mode web volatile-highlights use-package undo-tree tuareg swiper swap-buffers smex smart-mode-line shrink-whitespace rainbow-mode pixie-mode paradox org-plus-contrib org-cliplink org-bullets nyan-mode nginx-mode monroe material-theme markdown-mode magit json-mode js2-refactor intero inf-ruby ido-vertical-mode ido-ubiquitous hlinum highlight-parentheses helm-projectile helm-flycheck guide-key git-gutter-fringe gist flycheck-tip flycheck-pos-tip flycheck-color-mode-line flycheck-clojure flx-ido f expand-region exec-path-from-shell ethan-wspace enh-ruby-mode discover-my-major dired+ company-try-hard company-tern company-quickhelp company-emoji clj-refactor chruby beacon anzu alchemist ag ace-window)))
+ '(paradox-github-token t)
  '(rng-nxml-auto-validate-flag nil)
+ '(rspec-use-chruby t)
  '(rspec-use-rake-when-possible nil)
  '(rspec-use-spring-when-possible nil)
  '(rspec-use-zeus-when-possible nil)
@@ -39,4 +45,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(git-gutter:unchanged ((t (:background "brightblack")))))
