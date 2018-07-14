c0c8f365903558c066cf08ec7c18593b554976c0
Brian Cobb <bcobb@uwalumni.com>
2018-07-14 16:44:52 -0400

    Add chruby scripts

---
+++ bcobb/bcobb-chruby.el
@@ -0,0 +1,27 @@
+(use-package chruby)
+
+(defun bcobb-ruby-version (path)
+  (car (with-temp-buffer
+         (insert-file-contents path)
+         (split-string (buffer-string) "\n" t))))
+
+(defun bcobb-projectile-chruby ()
+  (let ((file-path (concat (projectile-project-root) ".ruby-version")))
+    (if (file-exists-p file-path)
+        (chruby (bcobb-ruby-version file-path))
+      "no .ruby-version in the project root")))
+
+(defun bcobb-cwd-chruby ()
+  (let ((file-path (".ruby-version")))
+    (if (file-exists-p file-path)
+        (chruby (bcobb-ruby-version file-path))
+      "no .ruby-version in the current directory"))  )
+
+(defun bcobb-chruby ()
+  (if (projectile-project-p)
+      (bcobb-projectile-chruby)
+    (bcobb-chruby-cwd)))
+
+(add-hook 'projectile-after-switch-project-hook 'bcobb-projectile-chruby)
+
+(provide 'bcobb-chruby)
