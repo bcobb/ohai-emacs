1cbfa9d64836fa8838d4af4e4f94dbeabb5f6099
Brian Cobb <bcobb@uwalumni.com>
2018-07-14 21:06:19 -0400

    Add enhanced ruby major mode

---
+++ bcobb/bcobb-ruby.el
@@ -0,0 +1,7 @@
+(use-package enh-ruby-mode)
+
+(add-to-list 'auto-mode-alist
+             '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+
+(provide 'bcobb-ruby)
--- custom.el
+++ custom.el
@@ -8,6 +8,7 @@
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" default)))
  '(ffap-machine-p-known (quote reject))
+ '(flycheck-disabled-checkers (quote (chef-foodcritic)))
  '(ido-vertical-define-keys (quote C-n-and-C-p-only))
  '(lsp-javascript-flow-server "flow")
  '(lsp-javascript-flow-server-args (quote ("lsp")))
@@ -22,7 +23,7 @@
     (ohai-appearance ohai-fonts ohai-general ohai-splash ohai-ido ohai-navigation ohai-editing ohai-complete ohai-snippets ohai-codestyle ohai-dired ohai-project ohai-flycheck ohai-git ohai-orgmode ohai-help ohai-elisp ohai-eshell ohai-unicode ohai-smart-mode-line ohai-ext-window-nav ohai-lsp ohai-html ohai-markdown ohai-js-web-mode)))
  '(package-selected-packages
    (quote
-    (undo-tree add-node-modules-path company-flow flycheck-flow emacs-flycheck-flow flow-minor-mode lsp-javascript-typescript prettier-js swiper ivy lsp-javascript-flow unicode-fonts chruby ag flow-mode tern js2-refactor js2-mode json-mode rainbow-mode web-mode lsp-ui company-lsp lsp-mode smart-mode-line rich-minority fish-completion bash-completion esh-autosuggest eshell-git-prompt eros highlight-parentheses paredit discover-my-major which-key org-cliplink org-bullets org-plus-contrib git-gutter-fringe gist magit flycheck-color-mode-line flycheck ibuffer-projectile projectile ethan-wspace yasnippet company-try-hard company-emoji company-quickhelp company volatile-highlights shrink-whitespace expand-region multiple-cursors anzu ace-window avy flx-ido ido-vertical-mode smex ido-completing-read+ web material-theme diminish nyan-mode hlinum exec-path-from-shell f use-package paradox)))
+    (enh-ruby-mode undo-tree add-node-modules-path company-flow flycheck-flow emacs-flycheck-flow flow-minor-mode lsp-javascript-typescript prettier-js swiper ivy lsp-javascript-flow unicode-fonts chruby ag flow-mode tern js2-refactor js2-mode json-mode rainbow-mode web-mode lsp-ui company-lsp lsp-mode smart-mode-line rich-minority fish-completion bash-completion esh-autosuggest eshell-git-prompt eros highlight-parentheses paredit discover-my-major which-key org-cliplink org-bullets org-plus-contrib git-gutter-fringe gist magit flycheck-color-mode-line flycheck ibuffer-projectile projectile ethan-wspace yasnippet company-try-hard company-emoji company-quickhelp company volatile-highlights shrink-whitespace expand-region multiple-cursors anzu ace-window avy flx-ido ido-vertical-mode smex ido-completing-read+ web material-theme diminish nyan-mode hlinum exec-path-from-shell f use-package paradox)))
  '(projectile-switch-project-action (quote projectile-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
