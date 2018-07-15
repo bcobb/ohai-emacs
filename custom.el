f8f46c4b04fcf22f237ff1a371349dc2bf668eca
Brian Cobb <bcobb@uwalumni.com>
2018-07-15 12:11:23 -0400

    Put logbook files in iCloud Drive

--- custom.el
+++ custom.el
@@ -10,6 +10,7 @@
  '(ffap-machine-p-known (quote reject))
  '(flycheck-disabled-checkers (quote (chef-foodcritic)))
  '(ido-vertical-define-keys (quote C-n-and-C-p-only))
+ '(logbook-directory "~/Library/Mobile Documents/com~apple~CloudDocs/logbook")
  '(lsp-javascript-flow-server "flow")
  '(lsp-javascript-flow-server-args (quote ("lsp")))
  '(ohai-personal-taste/paredit t)
@@ -25,7 +26,9 @@
    (quote
     (flycheck-yamllint yaml-mode rspec-mode enh-ruby-mode undo-tree add-node-modules-path company-flow flycheck-flow emacs-flycheck-flow flow-minor-mode lsp-javascript-typescript prettier-js swiper ivy lsp-javascript-flow unicode-fonts chruby ag flow-mode tern js2-refactor js2-mode json-mode rainbow-mode web-mode lsp-ui company-lsp lsp-mode smart-mode-line rich-minority fish-completion bash-completion esh-autosuggest eshell-git-prompt eros highlight-parentheses paredit discover-my-major which-key org-cliplink org-bullets org-plus-contrib git-gutter-fringe gist magit flycheck-color-mode-line flycheck ibuffer-projectile projectile ethan-wspace yasnippet company-try-hard company-emoji company-quickhelp company volatile-highlights shrink-whitespace expand-region multiple-cursors anzu ace-window avy flx-ido ido-vertical-mode smex ido-completing-read+ web material-theme diminish nyan-mode hlinum exec-path-from-shell f use-package paradox)))
  '(projectile-switch-project-action (quote projectile-dired))
- '(rspec-autosave-buffer t))
+ '(rspec-autosave-buffer t)
+ '(safe-local-variable-values (quote ((projectile-project-name . "logbook"))))
+ '(visible-bell nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
