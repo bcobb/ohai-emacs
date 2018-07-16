ec4f447039aa8bf06d47a376a0c8511790a81230
Brian Cobb <bcobb@uwalumni.com>
2018-07-16 12:02:25 -0400

    Disable scss flycheck
    In practice, the SCSS I work on only works in the context of a rails
    app so this checker isn't as useful as it could be.

--- custom.el
+++ custom.el
@@ -9,7 +9,7 @@
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" default)))
  '(dired-use-ls-dired nil)
  '(ffap-machine-p-known (quote reject))
- '(flycheck-disabled-checkers (quote (chef-foodcritic)))
+ '(flycheck-disabled-checkers (quote (scss chef-foodcritic)))
  '(ido-vertical-define-keys (quote C-n-and-C-p-only))
  '(logbook-directory "~/Library/Mobile Documents/com~apple~CloudDocs/logbook")
  '(lsp-javascript-flow-server "flow")
