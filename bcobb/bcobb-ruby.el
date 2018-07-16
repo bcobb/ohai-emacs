bbc797dea60de72d5296623f6a0f13f714a7d14a
Brian Cobb <bcobb@uwalumni.com>
2018-07-16 10:50:26 -0400

    Ruby: Add rufo integration

--- bcobb/bcobb-ruby.el
+++ bcobb/bcobb-ruby.el
@@ -1,8 +1,12 @@
 (use-package enh-ruby-mode)
 (use-package rspec-mode)
+(use-package rufo
+  :diminish 'rufo-minor-mode)
 
 (add-to-list 'auto-mode-alist
              '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
 (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
+
+(add-hook 'enh-ruby-mode-hook 'rufo-minor-mode)
 
 (provide 'bcobb-ruby)
