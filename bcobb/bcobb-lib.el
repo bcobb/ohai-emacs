96188ed82abb556feb96e44521c9017a557b5d69
Brian Cobb <bcobb@uwalumni.com>
2018-07-22 11:09:52 -0400

    TIL about `M-x describe-variable`

--- bcobb/bcobb-lib.el
+++ bcobb/bcobb-lib.el
@@ -3,9 +3,4 @@
   (apply 'message args)
   (setq inhibit-message nil))
 
-(defun inspect-exec-path ()
-  (debug-message "---- exec-path ----")
-  (dolist (el exec-path) (debug-message el))
-  (debug-message "---- o ----"))
-
 (provide 'bcobb-lib)
